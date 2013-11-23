606411e9de754a8f69d13e32922c29fff54ebe9d
Hugo Peixoto <hugo.peixoto@gmail.com>
2013-11-23 01:05:07 +0000

    Adds scope test

--- test/courgette/test_scope.rb
+++ test/courgette/test_scope.rb
@@ -7,10 +7,25 @@ class TestCourgetteScope < MiniTest::Unit::TestCase
   end
 
   def test_const
-    assert_equal [:Name], @scope.scope [:const, :Name]
+    assert_equal [:Name], @scope.scope([:const, :Name])
   end
 
-  def test_fail
-    assert false
+  def test_other
+    assert_equal [:potato], @scope.scope(:potato)
+  end
+
+  def test_colon2
+    assert_equal [:Scoped, :Name],
+      @scope.scope([:colon2, :Scoped, :Name])
+  end
+
+  def test_compose
+    assert_equal [:Really, :Scoped, :Name],
+      @scope.scope(
+        [:colon2,
+          [:colon2,
+            [:const, :Really],
+            [:const, :Scoped]],
+          [:const, :Name]])
   end
 end
